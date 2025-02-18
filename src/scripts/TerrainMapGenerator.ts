acd4b9f8b3d406b91a94a2caa38598296cef59a9
SunnyBoyWTF <Tylercoolguy1234@gmail.com>
2025-02-18 08:16:36 -0500

    Update TerrainMapGenerator.ts

--- src/scripts/TerrainMapGenerator.ts
+++ src/scripts/TerrainMapGenerator.ts
@@ -7,7 +7,7 @@ import { fileURLToPath } from "url";
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
 
-const mapName = "Europe";
+const mapName = "Antarctica.png";
 
 interface Coord {
   x: number;
