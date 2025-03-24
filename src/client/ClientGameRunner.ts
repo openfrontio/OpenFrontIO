9835cca0f95f4e6bd923ac0cee25b1b34274e169
Aotumuri <Aotumuri.yt@gmail.com>
2025-03-24 19:44:04 +0900

    added random botname

--- src/client/ClientGameRunner.ts
+++ src/client/ClientGameRunner.ts
@@ -34,6 +34,11 @@ import { UserSettings } from "../core/game/UserSettings";
 import { LocalPersistantStats } from "./LocalPersistantStats";
 import { createGameRecord } from "../core/Util";
 import { getPersistentIDFromCookie } from "./Main";
+import {
+  BOT_NAME_PREFIXES,
+  BOT_NAME_SUFFIXES,
+} from "../core/execution/utils/BotNames";
+import { PseudoRandom } from "../core/PseudoRandom";
 
 export interface LobbyConfig {
   serverConfig: ServerConfig;
@@ -156,6 +161,8 @@ export class ClientGameRunner {
     private gameView: GameView,
   ) {}
 
+  private random: PseudoRandom;
+
   private saveGame(update: WinUpdate) {
     const players: PlayerRecord[] = [
       {
@@ -181,6 +188,9 @@ export class ClientGameRunner {
 
   public start() {
     consolex.log("starting client game");
+
+    const displayNameMap: Record<string, string> = {};
+
     this.isActive = true;
     this.eventBus.on(MouseUpEvent, (e) => this.inputEvent(e));
 
@@ -264,6 +274,16 @@ export class ClientGameRunner {
       }
     };
     this.transport.connect(onconnect, onmessage);
+
+    const randomBotName = () => {
+      const prefixIndex = this.random.nextInt(0, BOT_NAME_PREFIXES.length);
+      const suffixIndex = this.random.nextInt(0, BOT_NAME_SUFFIXES.length);
+      return `${BOT_NAME_PREFIXES[prefixIndex]} ${BOT_NAME_SUFFIXES[suffixIndex]}`;
+    };
+
+    // this.gameView.players().forEach((player) => {
+    //   displayNameMap[player.name()] = `★${randomBotName()}`;
+    // });
   }
 
   public stop(saveFullGame: boolean = false) {
