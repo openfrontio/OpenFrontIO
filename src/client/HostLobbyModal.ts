400c92b7f3578cef69a7ad798db8dd34f51c0487
Aotumuri <Aotumuri.yt@gmail.com>
2025-03-25 09:08:33 +0900

    Formatting with npm run format

--- src/client/HostLobbyModal.ts
+++ src/client/HostLobbyModal.ts
@@ -46,24 +46,26 @@ export class HostLobbyModal extends LitElement {
             ?disabled=${this.copySuccess}
           >
             <span class="lobby-id">${this.lobbyId}</span>
-            ${this.copySuccess
-              ? html`<span class="copy-success-icon">✓</span>`
-              : html`
-                  <svg
-                    class="clipboard-icon"
-                    stroke="currentColor"
-                    fill="currentColor"
-                    stroke-width="0"
-                    viewBox="0 0 512 512"
-                    height="18px"
-                    width="18px"
-                    xmlns="http://www.w3.org/2000/svg"
-                  >
-                    <path
-                      d="M296 48H176.5C154.4 48 136 65.4 136 87.5V96h-7.5C106.4 96 88 113.4 88 135.5v288c0 22.1 18.4 40.5 40.5 40.5h208c22.1 0 39.5-18.4 39.5-40.5V416h8.5c22.1 0 39.5-18.4 39.5-40.5V176L296 48zm0 44.6l83.4 83.4H296V92.6zm48 330.9c0 4.7-3.4 8.5-7.5 8.5h-208c-4.4 0-8.5-4.1-8.5-8.5v-288c0-4.1 3.8-7.5 8.5-7.5h7.5v255.5c0 22.1 10.4 32.5 32.5 32.5H344v7.5zm48-48c0 4.7-3.4 8.5-7.5 8.5h-208c-4.4 0-8.5-4.1-8.5-8.5v-288c0-4.1 3.8-7.5 8.5-7.5H264v128h128v167.5z"
-                    ></path>
-                  </svg>
-                `}
+            ${
+              this.copySuccess
+                ? html`<span class="copy-success-icon">✓</span>`
+                : html`
+                    <svg
+                      class="clipboard-icon"
+                      stroke="currentColor"
+                      fill="currentColor"
+                      stroke-width="0"
+                      viewBox="0 0 512 512"
+                      height="18px"
+                      width="18px"
+                      xmlns="http://www.w3.org/2000/svg"
+                    >
+                      <path
+                        d="M296 48H176.5C154.4 48 136 65.4 136 87.5V96h-7.5C106.4 96 88 113.4 88 135.5v288c0 22.1 18.4 40.5 40.5 40.5h208c22.1 0 39.5-18.4 39.5-40.5V416h8.5c22.1 0 39.5-18.4 39.5-40.5V176L296 48zm0 44.6l83.4 83.4H296V92.6zm48 330.9c0 4.7-3.4 8.5-7.5 8.5h-208c-4.4 0-8.5-4.1-8.5-8.5v-288c0-4.1 3.8-7.5 8.5-7.5h7.5v255.5c0 22.1 10.4 32.5 32.5 32.5H344v7.5zm48-48c0 4.7-3.4 8.5-7.5 8.5h-208c-4.4 0-8.5-4.1-8.5-8.5v-288c0-4.1 3.8-7.5 8.5-7.5H264v128h128v167.5z"
+                      ></path>
+                    </svg>
+                  `
+            }
           </button>
         </div>
         <div class="options-layout">
@@ -80,17 +82,17 @@ export class HostLobbyModal extends LitElement {
                         .mapKey=${key}
                         .selected=${!this.useRandomMap &&
                         this.selectedMap === value}
-												.translation=${translateText(
-                            `map.${key.toLowerCase()}`,
+                        .translation=${translateText(
+                          `map.${key.toLowerCase()}`,
                         )}
                       ></map-display>
                     </div>
                   `,
                 )}
               <div
-                class="option-card random-map ${this.useRandomMap
-                  ? "selected"
-                  : ""}"
+                class="option-card random-map ${
+                  this.useRandomMap ? "selected" : ""
+                }"
                 @click=${this.handleRandomMapToggle}
               >
                 <div class="option-image">
@@ -123,9 +125,9 @@ export class HostLobbyModal extends LitElement {
                         .difficultyKey=${key}
                       ></difficulty-display>
                       <p class="option-card-title">
-                          ${translateText(
-                            `difficulty.${DifficultyDescription[key]}`,
-                          )}
+                        ${translateText(
+                          `difficulty.${DifficultyDescription[key]}`,
+                        )}
                       </p>
                     </div>
                   `,
@@ -151,10 +153,11 @@ export class HostLobbyModal extends LitElement {
                     .value="${String(this.bots)}"
                   />
                   <div class="option-card-title">
-                    <span>${translateText("host_modal.bots")}</span>${this
-                      .bots == 0
-                      ? translateText("host_modal.bots_disabled")
-                      : this.bots}
+                    <span>${translateText("host_modal.bots")}</span>${
+                      this.bots == 0
+                        ? translateText("host_modal.bots_disabled")
+                        : this.bots
+                    }
                   </div>
                 </label>
 
@@ -245,9 +248,11 @@ export class HostLobbyModal extends LitElement {
             <div class="options-section">
               <div class="option-title">
                 ${this.players.length}
-                ${this.players.length === 1
-                  ? translateText("host_modal.player")
-                  : translateText("host_modal.players")}
+                ${
+                  this.players.length === 1
+                    ? translateText("host_modal.player")
+                    : translateText("host_modal.players")
+                }
               </div>
 
             <div class="players-list">
@@ -262,9 +267,11 @@ export class HostLobbyModal extends LitElement {
             ?disabled=${this.players.length < 2}
             class="start-game-button"
           >
-            ${this.players.length === 1
-              ? translateText("host_modal.waiting")
-              : translateText("host_modal.start")}
+            ${
+              this.players.length === 1
+                ? translateText("host_modal.waiting")
+                : translateText("host_modal.start")
+            }
           </button>
         </div>
       </o-modal>
