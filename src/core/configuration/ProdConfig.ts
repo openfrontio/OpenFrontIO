6d3f4bdc8c257b487f13f58c337c756fe1aec686
Gaeritag <gaerita@gmail.com>
2025-04-10 23:08:01 +0200

    fixing last bug (fr this time)

--- src/core/configuration/ProdConfig.ts
+++ src/core/configuration/ProdConfig.ts
@@ -5,6 +5,9 @@ export const prodConfig = new (class extends DefaultServerConfig {
   r2Bucket(): string {
     return "openfront-staging";
   }
+  adminToken(): string {
+    return "WARNING_DEV_ADMIN_KEY_DO_NOT_USE_IN_PRODUCTION";
+  }
   numWorkers(): number {
     return 6;
   }
