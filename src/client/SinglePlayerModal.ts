9688381d7ce269946838668d4edddfb17f5c41fb
michaelabilliot <michaelabilliot@gmail.com>
2025-04-11 05:46:14 +0300

    Format Fix

--- src/client/SinglePlayerModal.ts
+++ src/client/SinglePlayerModal.ts
@@ -34,7 +34,11 @@ const mapCategories: Record<string, GameMapType[]> = {
     GameMapType.Mena,
     GameMapType.Australia,
   ],
-  fantasy: [GameMapType.Pangaea, GameMapType.Mars, GameMapType.KnownWorld],
+  fantasy: [
+    GameMapType.Pangaea, 
+    GameMapType.Mars, 
+    GameMapType.KnownWorld,
+  ],
 };
 
 @customElement("single-player-modal")
