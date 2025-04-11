1711c12987f52eca113dc67e4a9b2ad979feca83
Jonathan PHILIPPE <pretrine@gmail.com>
2025-04-11 23:04:26 +0200

    Adds statistics on collapsible buildings

--- src/client/graphics/layers/BuildingsStatsOverlay.ts
+++ src/client/graphics/layers/BuildingsStatsOverlay.ts
@@ -14,6 +14,9 @@ import portIcon from "../../../../resources/images/PortIcon.svg";
 import samLauncherIcon from "../../../../resources/images/SamLauncherIconWhite.svg";
 import shieldIcon from "../../../../resources/images/ShieldIconWhite.svg";
 
+import chevronLeftIcon from "../../../../resources/images/ChevronLeftWhite.svg";
+import chevronRightIcon from "../../../../resources/images/ChevronRightWhite.svg";
+
 @customElement("buildings-stats-overlay")
 export class BuildingsStatsOverlay extends LitElement implements Layer {
   @property({ type: Object })
@@ -24,6 +27,9 @@ export class BuildingsStatsOverlay extends LitElement implements Layer {
 
   @state()
   private _isVisible = false;
+
+  @state()
+  private _isCollapsing = false;
 
   @state()
   private _gold: number;
@@ -85,6 +91,10 @@ export class BuildingsStatsOverlay extends LitElement implements Layer {
     this.requestUpdate();
   }
 
+  toggleCollapse() {
+    this._isCollapsing = !this._isCollapsing;
+  }
+
   private renderIcon(src: string) {
     return html`<img class="p-2 w-10 h-10" src="${src}" />`;
   }
@@ -95,6 +105,45 @@ export class BuildingsStatsOverlay extends LitElement implements Layer {
       <div class="flex flex-row items-center gap-2 opacity-80" translate="no">
         ${iconTag}
         <span class="py-2 pr-2">${renderNumber(count)}</span>
+      </div>
+    `;
+  }
+
+  renderChevron() {
+    return html`
+      <button
+        class="flex items-center justify-center p-1
+                               bg-opacity-70 bg-gray-700 text-opacity-90 text-white
+                               border-none rounded cursor-pointer
+                               hover:bg-opacity-60 hover:bg-gray-600
+                               transition-colors duration-200
+                               text-sm lg:text-xl pointer-events-auto"
+        @click=${this.toggleCollapse}
+      >
+        <img
+          class="p-1 h-6"
+          src="${this._isCollapsing ? chevronLeftIcon : chevronRightIcon}"
+        />
+      </button>
+    `;
+  }
+
+  private renderBuildingStats() {
+    const containerClasses = this._isCollapsing
+      ? "opacity-100 visible"
+      : "opacity-0 invisible w-0 pointer-events-none";
+
+    return html`
+      <div
+        class="flex ${containerClasses} ease-in duration-200"
+        style="transition-property: width, opacity"
+      >
+        ${this.renderResourceCount(warshipIcon, this._warships)}
+        ${this.renderResourceCount(portIcon, this._ports)}
+        ${this.renderResourceCount(cityIcon, this._cities)}
+        ${this.renderResourceCount(shieldIcon, this._defensePosts)}
+        ${this.renderResourceCount(samLauncherIcon, this._samLaunchers)}
+        ${this.renderResourceCount(missileSiloIcon, this._missileSilos)}
       </div>
     `;
   }
@@ -116,17 +165,12 @@ export class BuildingsStatsOverlay extends LitElement implements Layer {
               translate="no"
             >
               ${this.renderIcon(goldCoinIcon)}
-              <span class="py-2 pr-2"
-                >${renderNumber(this._gold)}
+              <span class="py-2 pr-2">
+                ${renderNumber(this._gold)}
                 (+${renderNumber(this._goldPerSecond)})</span
               >
             </div>
-            ${this.renderResourceCount(warshipIcon, this._warships)}
-            ${this.renderResourceCount(portIcon, this._ports)}
-            ${this.renderResourceCount(cityIcon, this._cities)}
-            ${this.renderResourceCount(shieldIcon, this._defensePosts)}
-            ${this.renderResourceCount(samLauncherIcon, this._samLaunchers)}
-            ${this.renderResourceCount(missileSiloIcon, this._missileSilos)}
+            ${this.renderBuildingStats()} ${this.renderChevron()}
           </div>
         </div>
       </div>
