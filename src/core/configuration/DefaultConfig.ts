dd44f094fb48a3c64381e5aa350413997c4d82b4
Brandon Yi <yimail.brandon@gmail.com>
2025-04-14 22:30:51 -0700

    Decrease SAM cost
    Related https://github.com/openfrontio/OpenFrontIO/issues/515

--- src/core/configuration/DefaultConfig.ts
+++ src/core/configuration/DefaultConfig.ts
@@ -322,10 +322,10 @@ export class DefaultConfig implements Config {
             p.type() == PlayerType.Human && this.infiniteGold()
               ? 0
               : Math.min(
-                  1_500_000 * 3,
+                  1_250_000 * 3,
                   (p.unitsIncludingConstruction(UnitType.SAMLauncher).length +
                     1) *
-                    1_500_000,
+                    1_250_000,
                 ),
           territoryBound: true,
           constructionDuration: this.instantBuild() ? 0 : 30 * 10,
