901de04f62b8378c40f103831a47060f420a17b5
Duwibi <86431918+Duwibi@users.noreply.github.com>
2025-05-11 20:07:59 +0000

    Update Lobby Sizes by map

--- src/core/configuration/DefaultConfig.ts
+++ src/core/configuration/DefaultConfig.ts
@@ -80,63 +80,78 @@ export abstract class DefaultServerConfig implements ServerConfig {
 
   lobbyMaxPlayers(map: GameMapType, mode: GameMode): number {
     const numPlayers = () => {
-      // Maps with ~4 mil pixels
-      if (
-        [
-          GameMapType.GatewayToTheAtlantic,
-          GameMapType.SouthAmerica,
-          GameMapType.NorthAmerica,
-          GameMapType.Africa,
-          GameMapType.Europe,
-        ].includes(map)
-      ) {
+      if (GameMapType.GatewayToTheAtlantic.includes(map)) {
+        return Math.random() < 0.2 ? 80 : 50;
+      }
+      if (GameMapType.SouthAmerica.includes(map)) {
+        return Math.random() < 0.2 ? 70 : 40;
+      }
+      if (GameMapType.NorthAmerica.includes(map)) {
+        return Math.random() < 0.2 ? 80 : 50;
+      }
+      if (GameMapType.Africa.includes(map)) {
         return Math.random() < 0.2 ? 100 : 50;
       }
-      // Maps with ~2.5 - ~3.5 mil pixels
-      if (
-        [
-          GameMapType.Australia,
-          GameMapType.Iceland,
-          GameMapType.Britannia,
-          GameMapType.Asia,
-          GameMapType.FalklandIslands,
-          GameMapType.Baikal,
-        ].includes(map)
-      ) {
-        return Math.random() < 0.3 ? 50 : 25;
+      if (GameMapType.Europe.includes(map)) {
+        return Math.random() < 0.2 ? 80 : 50;
       }
-      // Maps with ~2 mil pixels
-      if (
-        [
-          GameMapType.Mena,
-          GameMapType.Mars,
-          GameMapType.Oceania,
-          GameMapType.Japan, // Japan at this level because its 2/3 water
-          GameMapType.FaroeIslands,
-          GameMapType.DeglaciatedAntarctica,
-          GameMapType.EuropeClassic,
-        ].includes(map)
-      ) {
-        return Math.random() < 0.3 ? 50 : 25;
+      if (GameMapType.Australia.includes(map)) {
+        return Math.random() < 0.2 ? 50 : 30;
       }
-      // Maps smaller than ~2 mil pixels
-      if (
-        [
-          GameMapType.BetweenTwoSeas,
-          GameMapType.BlackSea,
-          GameMapType.Pangaea,
-        ].includes(map)
-      ) {
-        return Math.random() < 0.5 ? 30 : 15;
+      if (GameMapType.Iceland.includes(map)) {
+        return Math.random() < 0.2 ? 50 : 30;
       }
-      // world belongs with the ~2 mils, but these amounts never made sense so I assume the insanity is intended.
-      if (map == GameMapType.World) {
+      if (GameMapType.Britannia.includes(map)) {
+        return Math.random() < 0.2 ? 50 : 30;
+      }
+      if (GameMapType.Asia.includes(map)) {
+        return Math.random() < 0.2 ? 60 : 30;
+      }
+      if (GameMapType.FalklandIslands.includes(map)) {
+        return Math.random() < 0.2 ? 50 : 30;
+      }
+      if (GameMapType.Baikal.includes(map)) {
+        return Math.random() < 0.2 ? 60 : 40;
+      }
+      if (GameMapType.Mena.includes(map)) {
+        return Math.random() < 0.2 ? 60 : 30;
+      }
+      if (GameMapType.Mars.includes(map)) {
+        return Math.random() < 0.2 ? 50 : 30;
+      }
+      if (GameMapType.Oceania.includes(map)) {
+        return Math.random() < 0.2 ? 30 : 20;
+      }
+      if (GameMapType.Japan.includes(map)) {
+        return Math.random() < 0.2 ? 50 : 30;
+      }
+      if (GameMapType.FaroeIslands.includes(map)) {
+        return Math.random() < 0.2 ? 40 : 30;
+      }
+      if (GameMapType.DeglaciatedAntarctica.includes(map)) {
+        return Math.random() < 0.2 ? 50 : 30;
+      }
+      if (GameMapType.EuropeClassic.includes(map)) {
+        return Math.random() < 0.2 ? 80 : 50;
+      }
+      if (GameMapType.BetweenTwoSeas.includes(map)) {
+        return Math.random() < 0.2 ? 40 : 30;
+      }
+      if (GameMapType.BlackSea.includes(map)) {
+        return Math.random() < 0.2 ? 40 : 30;
+      }
+      if (GameMapType.Pangaea.includes(map)) {
+        return Math.random() < 0.2 ? 40 : 30;
+      }
+      if (GameMapType.World.includes(map)) {
         return Math.random() < 0.2 ? 150 : 50;
       }
-      // default return for non specified map
+      if (GameMapType.KnownWorld.includes(map)) {
+        return Math.random() < 0.2 ? 50 : 30;
+      }
       return Math.random() < 0.2 ? 50 : 20;
     };
-    return Math.min(150, numPlayers() * (mode == GameMode.Team ? 2 : 1));
+    return Math.min(150, numPlayers() * (mode == GameMode.Team ? 1.5 : 1));
   }
 
   workerIndex(gameID: GameID): number {
