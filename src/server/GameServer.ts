3ff609e95575c1f4686e8f68e7b150e06b1ac7d0
Scott Anderson <662325+scottanderson@users.noreply.github.com>
2025-05-19 02:27:39 -0400

    playerid

--- src/server/GameServer.ts
+++ src/server/GameServer.ts
@@ -386,7 +386,7 @@ export class GameServer {
         const playerRecords: PlayerRecord[] = Array.from(
           this.allClients.values(),
         ).map((client) => {
-          const stats = this.winner?.allPlayersStats[client.clientID];
+          const stats = this.winner?.allPlayersStats[client.playerID];
           if (stats === undefined) throw new Error();
           return {
             ip: ipAnonymize(client.ip),
