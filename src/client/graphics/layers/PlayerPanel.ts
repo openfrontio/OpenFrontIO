11dce0a6c4156eae888fe603d203d9b71b6665be
tnhnblgl <51187395+tnhnblgl@users.noreply.github.com>
2025-05-20 00:03:32 +0300

    Prettier

--- src/client/graphics/layers/PlayerPanel.ts
+++ src/client/graphics/layers/PlayerPanel.ts
@@ -337,18 +337,22 @@ export class PlayerPanel extends LitElement implements Layer {
                 </div>
               </div>
 
-            ${this.allianceExpiryText !== null
-              ? html`
-                  <div class="flex flex-col gap-1">
-                    <div class="text-white text-opacity-80 text-sm px-2">
-                      ${translateText("player_panel.alliance_time_remaining")}
+            ${
+              this.allianceExpiryText !== null
+                ? html`
+                    <div class="flex flex-col gap-1">
+                      <div class="text-white text-opacity-80 text-sm px-2">
+                        ${translateText("player_panel.alliance_time_remaining")}
+                      </div>
+                      <div
+                        class="bg-opacity-50 bg-gray-700 rounded p-2 text-white"
+                      >
+                        ${this.allianceExpiryText}
+                      </div>
                     </div>
-                    <div class="bg-opacity-50 bg-gray-700 rounded p-2 text-white">
-                      ${this.allianceExpiryText}
-                    </div>
-                  </div>
-                `
-              : ""}
+                  `
+                : ""
+            }
               <!-- Stats -->
               <div class="flex flex-col gap-1">
                 <div class="text-white text-opacity-80 text-sm px-2">
