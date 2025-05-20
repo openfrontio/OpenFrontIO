6fa6c5b58dc32a0c958029ccbba5bd0c25cde9a4
tnhnblgl <51187395+tnhnblgl@users.noreply.github.com>
2025-05-20 23:50:05 +0300

    Update username.ts

--- src/core/validations/username.ts
+++ src/core/validations/username.ts
@@ -14,7 +14,7 @@ const matcher = new RegExpMatcher({
 export const MIN_USERNAME_LENGTH = 3;
 export const MAX_USERNAME_LENGTH = 27;
 
-const validPattern = /^[a-zA-Z0-9_\[\] 🐈🍀üÜ]+$/u;
+const validPattern = /^[\w \[\]\p{Emoji}]+$/u;
 
 const shadowNames = [
   "NicePeopleOnly",
