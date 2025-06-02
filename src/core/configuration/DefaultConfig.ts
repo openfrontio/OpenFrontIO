1cc4827a77b69c79c6871c270f3cd517b5e7f5f6
1brucben <1benjbruce@gmail.com>
2025-06-02 23:40:01 +0200

    nerf port gold again

--- src/core/configuration/DefaultConfig.ts
+++ src/core/configuration/DefaultConfig.ts
@@ -276,7 +276,7 @@ export class DefaultConfig implements Config {
     return 10000 + 150 * Math.pow(dist, 1.1);
   }
   tradeShipSpawnRate(numberOfPorts: number): number {
-    return Math.round(10 * Math.pow(numberOfPorts, 0.35));
+    return Math.round(10 * Math.pow(numberOfPorts, 0.38));
   }
 
   unitInfo(type: UnitType): UnitInfo {
