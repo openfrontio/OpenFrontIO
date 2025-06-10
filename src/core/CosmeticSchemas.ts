133313cbdee2b6e5a5122c2a3c9d34f27c1e5d63
Aotumuri <Aotumuri.YT@gmail.com>
2025-06-10 12:41:15 +0900

    format

--- src/core/CosmeticSchemas.ts
+++ src/core/CosmeticSchemas.ts
@@ -1,14 +1,14 @@
 import { z } from "zod";
 
 export const CosmeticsSchema = z.object({
-    role_group: z.record(z.string(), z.string().array()).optional(),
-    pattern: z.record(
-      z.string(),
-      z.object({
-        pattern: z.string().base64(),
-        role_group: z.string().array().optional(),
-      }),
-    ),
-  });
-  
-  export type Cosmetic = z.infer<typeof CosmeticsSchema>;
+  role_group: z.record(z.string(), z.string().array()).optional(),
+  pattern: z.record(
+    z.string(),
+    z.object({
+      pattern: z.string().base64(),
+      role_group: z.string().array().optional(),
+    }),
+  ),
+});
+
+export type Cosmetic = z.infer<typeof CosmeticsSchema>;
