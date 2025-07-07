d1b749ef8c54d3a5cef530e866ee8486160cce5b
Aotumuri <Aotumuri.YT@gmail.com>
2025-07-07 17:18:57 +0900

    add real flag selection modal

--- src/client/FlagInputModal.ts
+++ src/client/FlagInputModal.ts
@@ -1,5 +1,6 @@
 import { LitElement, html } from "lit";
-import { customElement, query } from "lit/decorators.js";
+import { customElement, query, state } from "lit/decorators.js";
+import Countries from "./data/countries.json";
 
 @customElement("flag-input-modal")
 export class FlagInputModal extends LitElement {
@@ -7,6 +8,8 @@ export class FlagInputModal extends LitElement {
     open: () => void;
     close: () => void;
   };
+
+  @state() private search: string = "";
 
   createRenderRoot() {
     return this;
@@ -18,14 +21,63 @@ export class FlagInputModal extends LitElement {
         <div class="text-center text-2xl font-bold my-4">
           Flag Selector Modal
         </div>
+        <input
+          class="h-[2rem] border-none text-center border border-gray-300 rounded-xl shadow-sm text-2xl text-center focus:outline-none focus:ring-2 focus:ring-blue-500 focus:border-blue-500 text-black dark:border-gray-300/60 dark:bg-gray-700 dark:text-white"
+          type="text"
+          placeholder="Search..."
+          @change=${this.handleSearch}
+          @keyup=${this.handleSearch}
+        />
+        <div
+          class="flex flex-wrap justify-evenly gap-[1rem] overflow-y-auto overflow-x-hidden h-[90%]"
+        >
+          ${Countries.filter(
+            (country) =>
+              country.name.toLowerCase().includes(this.search.toLowerCase()) ||
+              country.code.toLowerCase().includes(this.search.toLowerCase()),
+          ).map(
+            (country) => html`
+              <button
+                @click=${() => {
+                  this.setFlag(country.code);
+                  this.close();
+                }}
+                class="text-center cursor-pointer border-none bg-none opacity-70 
+                  w-[calc(100%/2-15px)] sm:w-[calc(100%/4-15px)] 
+                  md:w-[calc(100%/6-15px)] lg:w-[calc(100%/8-15px)] 
+                  xl:w-[calc(100%/10-15px)] min-w-[80px]"
+              >
+                <img
+                  class="country-flag w-full h-auto"
+                  src="/flags/${country.code}.svg"
+                />
+                <span class="country-name">${country.name}</span>
+              </button>
+            `,
+          )}
+        </div>
       </o-modal>
     `;
+  }
+
+  private handleSearch(event: Event) {
+    this.search = (event.target as HTMLInputElement).value;
+  }
+
+  private setFlag(flag: string) {
+    localStorage.setItem("flag", flag);
+    this.dispatchEvent(
+      new CustomEvent("flag-change", {
+        detail: { flag },
+        bubbles: true,
+        composed: true,
+      }),
+    );
   }
 
   public open() {
     this.modalEl?.open();
   }
-
   public close() {
     this.modalEl?.close();
   }
