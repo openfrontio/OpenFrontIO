b3c3b42bbcd5a4d5e9f5a16c191f451519ae8557
Scott Anderson <662325+scottanderson@users.noreply.github.com>
2025-07-09 01:24:27 -0400

    Clear cookie

--- src/client/jwt.ts
+++ src/client/jwt.ts
@@ -7,6 +7,7 @@ import {
   UserMeResponse,
   UserMeResponseSchema,
 } from "../core/ApiSchemas";
+import { getServerConfigFromClient } from "../core/configuration/ConfigLoader";
 
 function getAudience() {
   const { hostname } = new URL(window.location.href);
@@ -49,12 +50,21 @@ function getToken(): string | null {
     ?.trim()
     .substring(6);
   if (cookie !== undefined) {
-    // TODO: Replace localStorage with cookie
-    localStorage.setItem("token", cookie);
+    return cookie;
   }
 
   // Check local storage
   return localStorage.getItem("token");
+}
+
+async function clearToken() {
+  localStorage.removeItem("token");
+  __isLoggedIn = false;
+  const config = await getServerConfigFromClient();
+  const audience = config.jwtAudience();
+  const isSecure = window.location.protocol === "https:";
+  const secure = isSecure ? "; Secure" : "";
+  document.cookie = `token=logged_out; Path=/; Max-Age=0; Domain=${audience}${secure}`;
 }
 
 export function discordLogin() {
@@ -68,10 +78,9 @@ export function getAuthHeader(): string {
 }
 
 export async function logOut(allSessions: boolean = false) {
-  const token = localStorage.getItem("token");
+  const token = getToken();
   if (token === null) return;
-  localStorage.removeItem("token");
-  __isLoggedIn = false;
+  clearToken();
 
   const response = await fetch(
     getApiBase() + (allSessions ? "/revoke" : "/logout"),
@@ -191,8 +200,7 @@ export async function postRefresh(): Promise<boolean> {
       },
     });
     if (response.status === 401) {
-      localStorage.removeItem("token");
-      __isLoggedIn = false;
+      clearToken();
       return false;
     }
     if (response.status !== 200) return false;
@@ -223,8 +231,7 @@ export async function getUserMe(): Promise<UserMeResponse | false> {
       },
     });
     if (response.status === 401) {
-      localStorage.removeItem("token");
-      __isLoggedIn = false;
+      clearToken();
       return false;
     }
     if (response.status !== 200) return false;
