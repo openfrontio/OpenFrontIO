b575d0549630decc06bc95f1e06b0ddb2abcb968
Aotumuri <Aotumuri.YT@gmail.com>
2025-07-10 12:38:32 +0900

    fixed

--- src/client/graphics/layers/PlayerInfoOverlay.ts
+++ src/client/graphics/layers/PlayerInfoOverlay.ts
@@ -250,66 +250,86 @@ export class PlayerInfoOverlay extends LitElement implements Layer {
           ${translateText("player_info_overlay.gold")}:
           ${renderNumber(player.gold())}
         </div>
-        <div class="text-sm opacity-80" translate="no">
-          ${translateText("player_info_overlay.ports")}:
-          ${player.units(UnitType.Port).length}
-          ${player
-            .units(UnitType.Port)
-            .map((unit) => unit.level())
-            .reduce((a, b) => a + b, 0) > 1
-            ? html`(${translateText("player_info_overlay.levels")}:
-              ${player
-                .units(UnitType.Port)
-                .map((unit) => unit.level())
-                .reduce((a, b) => a + b, 0)})`
-            : ""}
-        </div>
-        <div class="text-sm opacity-80" translate="no">
-          ${translateText("player_info_overlay.cities")}:
-          ${player.units(UnitType.City).length}
-          ${player
-            .units(UnitType.City)
-            .map((unit) => unit.level())
-            .reduce((a, b) => a + b, 0) > 1
-            ? html`(${translateText("player_info_overlay.levels")}:
-              ${player
-                .units(UnitType.City)
-                .map((unit) => unit.level())
-                .reduce((a, b) => a + b, 0)})`
-            : ""}
-        </div>
-        <div class="text-sm opacity-80" translate="no">
-          ${translateText("player_info_overlay.missile_launchers")}:
-          ${player.units(UnitType.MissileSilo).length}
-          ${player
-            .units(UnitType.MissileSilo)
-            .map((unit) => unit.level())
-            .reduce((a, b) => a + b, 0) > 1
-            ? html`(${translateText("player_info_overlay.levels")}:
-              ${player
-                .units(UnitType.MissileSilo)
-                .map((unit) => unit.level())
-                .reduce((a, b) => a + b, 0)})`
-            : ""}
-        </div>
-        <div class="text-sm opacity-80" translate="no">
-          ${translateText("player_info_overlay.sams")}:
-          ${player.units(UnitType.SAMLauncher).length}
-          ${player
-            .units(UnitType.SAMLauncher)
-            .map((unit) => unit.level())
-            .reduce((a, b) => a + b, 0) > 1
-            ? html`(${translateText("player_info_overlay.levels")}:
-              ${player
-                .units(UnitType.SAMLauncher)
-                .map((unit) => unit.level())
-                .reduce((a, b) => a + b, 0)})`
-            : ""}
-        </div>
-        <div class="text-sm opacity-80" translate="no">
-          ${translateText("player_info_overlay.warships")}:
-          ${player.units(UnitType.Warship).length}
-        </div>
+        ${!this.game.config().isUnitDisabled(UnitType.Port)
+          ? html`
+              <div class="text-sm opacity-80" translate="no">
+                ${translateText("player_info_overlay.ports")}:
+                ${player.units(UnitType.Port).length}
+                ${player
+                  .units(UnitType.Port)
+                  .map((unit) => unit.level())
+                  .reduce((a, b) => a + b, 0) > 1
+                  ? html`(${translateText("player_info_overlay.levels")}:
+                    ${player
+                      .units(UnitType.Port)
+                      .map((unit) => unit.level())
+                      .reduce((a, b) => a + b, 0)})`
+                  : ""}
+              </div>
+            `
+          : ""}
+        ${!this.game.config().isUnitDisabled(UnitType.City)
+          ? html`
+              <div class="text-sm opacity-80" translate="no">
+                ${translateText("player_info_overlay.cities")}:
+                ${player.units(UnitType.City).length}
+                ${player
+                  .units(UnitType.City)
+                  .map((unit) => unit.level())
+                  .reduce((a, b) => a + b, 0) > 1
+                  ? html`(${translateText("player_info_overlay.levels")}:
+                    ${player
+                      .units(UnitType.City)
+                      .map((unit) => unit.level())
+                      .reduce((a, b) => a + b, 0)})`
+                  : ""}
+              </div>
+            `
+          : ""}
+        ${!this.game.config().isUnitDisabled(UnitType.MissileSilo)
+          ? html`
+              <div class="text-sm opacity-80" translate="no">
+                ${translateText("player_info_overlay.missile_launchers")}:
+                ${player.units(UnitType.MissileSilo).length}
+                ${player
+                  .units(UnitType.MissileSilo)
+                  .map((unit) => unit.level())
+                  .reduce((a, b) => a + b, 0) > 1
+                  ? html`(${translateText("player_info_overlay.levels")}:
+                    ${player
+                      .units(UnitType.MissileSilo)
+                      .map((unit) => unit.level())
+                      .reduce((a, b) => a + b, 0)})`
+                  : ""}
+              </div>
+            `
+          : ""}
+        ${!this.game.config().isUnitDisabled(UnitType.SAMLauncher)
+          ? html`
+              <div class="text-sm opacity-80" translate="no">
+                ${translateText("player_info_overlay.sams")}:
+                ${player.units(UnitType.SAMLauncher).length}
+                ${player
+                  .units(UnitType.SAMLauncher)
+                  .map((unit) => unit.level())
+                  .reduce((a, b) => a + b, 0) > 1
+                  ? html`(${translateText("player_info_overlay.levels")}:
+                    ${player
+                      .units(UnitType.SAMLauncher)
+                      .map((unit) => unit.level())
+                      .reduce((a, b) => a + b, 0)})`
+                  : ""}
+              </div>
+            `
+          : ""}
+        ${!this.game.config().isUnitDisabled(UnitType.Warship)
+          ? html`
+              <div class="text-sm opacity-80" translate="no">
+                ${translateText("player_info_overlay.warships")}:
+                ${player.units(UnitType.Warship).length}
+              </div>
+            `
+          : ""}
         ${relationHtml}
       </div>
     `;
