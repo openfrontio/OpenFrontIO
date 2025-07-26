625bd63ac9903ce241862782ce8acc94ad3f6b29
Scott Anderson <662325+scottanderson@users.noreply.github.com>
2025-07-26 02:10:03 -0400

    wip

--- src/global.d.ts
+++ src/global.d.ts
@@ -1,9 +1,8 @@
-import "@total-typescript/ts-reset";
-
 declare module "*.png" {
   const content: string;
   export default content;
 }
+
 declare module "*.jpg" {
   const value: string;
   export default value;
@@ -18,26 +17,32 @@ declare module "*.jpeg" {
   const value: string;
   export default value;
 }
+
 declare module "*.svg" {
   const value: string;
   export default value;
 }
+
 declare module "*.bin" {
   const value: string;
   export default value;
 }
+
 declare module "*.md" {
   const value: string;
   export default value;
 }
+
 declare module "*.txt" {
   const value: string;
   export default value;
 }
+
 declare module "*.html" {
   const content: string;
   export default content;
 }
+
 declare module "*.xml" {
   const value: string;
   export default value;
