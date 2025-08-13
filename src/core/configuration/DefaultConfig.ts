56c9457421bfae8f0d77e0970e8cf2f05c106857
evanpelle <evanpelle@gmail.com>
2025-08-13 11:51:01 -0700

    nerf bots

--- src/core/configuration/DefaultConfig.ts
+++ src/core/configuration/DefaultConfig.ts
@@ -744,7 +744,7 @@ export class DefaultConfig implements Config {
             this.cityTroopIncrease();
 
     if (player.type() === PlayerType.Bot) {
-      return maxTroops / 2;
+      return maxTroops / 3;
     }
 
     if (player.type() === PlayerType.Human) {
@@ -796,6 +796,9 @@ export class DefaultConfig implements Config {
   }
 
   goldAdditionRate(player: Player): Gold {
+    if (player.type() === PlayerType.Bot) {
+      return 50n;
+    }
     return 100n;
   }
 
