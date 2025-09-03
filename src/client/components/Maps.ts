52661976bf54b5ceb515be604d8d0116e6f20e20
Abdulrahman Yarub <abdyyt2007@gmail.com>
2025-09-03 10:26:46 +0300

    Refactor: Rename map files and folder to meet conventions, and update map strength

--- src/client/components/Maps.ts
+++ src/client/components/Maps.ts
@@ -36,6 +36,7 @@ export const MapDescription: Record<keyof typeof GameMapType, string> = {
   Italia: "Italia",
   Yenisei: "Yenisei",
   Pluto: "Pluto",
+  Middleeast: "Middle East",
 };
 
 @customElement("map-display")
