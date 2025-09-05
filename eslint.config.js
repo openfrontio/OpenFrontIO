a8679db17c137c6ea1c726bae3bc4d6cf585b5e3
Michal Martínek <96337712+Michal-Martinek@users.noreply.github.com>
2025-09-05 22:55:13 +0200

    Enabled `@typescript-eslint/no-unused-expressions` eslint rule

--- eslint.config.js
+++ eslint.config.js
@@ -40,7 +40,7 @@ export default [
     rules: {
       // Disable rules that would fail. The failures should be fixed, and the entries here removed.
       "@typescript-eslint/no-explicit-any": "off",
-      "@typescript-eslint/no-unused-expressions": "off",
+      "@typescript-eslint/no-unused-expressions": "error",
       "@typescript-eslint/no-unused-vars": "off",
       "no-case-declarations": "off",
     },
