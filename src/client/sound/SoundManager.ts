e8f03249130cb16808261c6cccc0b8c5be7f1c2d
icslucas <carolinacarazolli@gmail.com>
2025-10-26 15:02:47 +0100

    add new tracks

--- src/client/sound/SoundManager.ts
+++ src/client/sound/SoundManager.ts
@@ -2,6 +2,8 @@ import { Howl } from "howler";
 import of4 from "../../../proprietary/sounds/music/of4.mp3";
 import openfront from "../../../proprietary/sounds/music/openfront.mp3";
 import war from "../../../proprietary/sounds/music/war.mp3";
+import of2 from "../../../proprietary/sounds/music/of2.mp3";
+import evan from "../../../proprietary/sounds/music/evan.mp3";
 import kaChingSound from "../../../resources/sounds/effects/ka-ching.mp3";
 
 export enum SoundEffect {
@@ -25,6 +27,18 @@ class SoundManager {
       }),
       new Howl({
         src: [openfront],
+        loop: false,
+        onend: this.playNext.bind(this),
+        volume: 0,
+      }),
+      new Howl({
+        src: [evan],
+        loop: false,
+        onend: this.playNext.bind(this),
+        volume: 0,
+      }),
+      new Howl({
+        src: [of2],
         loop: false,
         onend: this.playNext.bind(this),
         volume: 0,
