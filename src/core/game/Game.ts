4470c3d58aaac9360240a1f1e8bc08772c2d7000
Evan <evanpelle@gmail.com>
2025-11-04 11:52:52 -0800

    Discourage trading with nearby ports (#2381)
    ## Description:
    
    The **proximityBonusPortsNb** function increased the likelihood a
    tradeship would go to a nearby port. But now that trade gold is nerfed
    from nearby ports, we shouldn't encourage trading with ports that are
    too close. So now add another factor **tradeShipShortRangeDebuff** That
    cancels out the proximity bonus if the port is too close.
    
    Now tradeships are encouraged to go to ports that are close, but not too
    close.
    
    Also move tradingPorts method to the PortExecution class because that's
    the only place it's used.
    
    ## Please complete the following:
    
    - [x] I have added screenshots for all UI updates
    - [x] I process any text displayed to the user through translateText()
    and I've added it to the en.json file
    - [x] I have added relevant tests to the test directory
    - [x] I confirm I have thoroughly tested these changes and take full
    responsibility for any bugs introduced
    
    ## Please put your Discord username so you can be contacted if a bug or
    regression is found:
    
    evan

--- src/core/game/Game.ts
+++ src/core/game/Game.ts
@@ -648,7 +648,6 @@ export interface Player {
   // Misc
   toUpdate(): PlayerUpdate;
   playerProfile(): PlayerProfile;
-  tradingPorts(port: Unit): Unit[];
   // WARNING: this operation is expensive.
   bestTransportShipSpawn(tile: TileRef): TileRef | false;
 }
