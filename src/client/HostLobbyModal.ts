cf08deaea0ae9b361ad836f7f9e8d10ea515123e
Abdallah Bahrawi <140177728+abdallahbahrawi1@users.noreply.github.com>
2025-11-20 05:27:41 +0200

    Lobby Team Preview UI  (#2444)
    Resolves #1092
    
    ## Description:
    
    Added a team preview to the Host Lobby: players listed on the left,
    teams on the right in two scrollable columns with color dots matching
    in-game colors. Implemented accurate server-parity team assignment
    (including clan grouping).
    
    Screenshots:
    
    <img width="817" height="519" alt="Screenshot 2025-11-13 173721"
    src="https://github.com/user-attachments/assets/ec646238-7efa-4c8f-9c0a-171b61fd3f20"
    />
    
    <img width="762" height="425" alt="Screenshot 2025-11-13 175400"
    src="https://github.com/user-attachments/assets/ebdccb80-4c07-41d5-8f69-3ea983d4b243"
    />
    
    
    ## Please complete the following:
    
    - [x] I have added screenshots for all UI updates
    - [x] I process any text displayed to the user through translateText()
    and I've added it to the en.json file
    - [x] I have added relevant tests to the test directory
    - [x] I confirm I have thoroughly tested these changes and take full
    responsibility for any bugs introduced
    
    ## Please put your Discord username so you can be contacted if a bug or
    regression is found:
    
    abodcraft1
    
    ---------
    
    Co-authored-by: Evan <evanpelle@gmail.com>

--- src/client/HostLobbyModal.ts
+++ src/client/HostLobbyModal.ts
@@ -25,6 +25,7 @@ import {
 import { generateID } from "../core/Util";
 import "./components/baseComponents/Modal";
 import "./components/Difficulties";
+import "./components/LobbyTeamView";
 import "./components/Maps";
 import { JoinLobbyEvent } from "./Main";
 import { renderUnitTypeOptions } from "./utilities/RenderUnitTypeOptions";
@@ -554,27 +555,13 @@ export class HostLobbyModal extends LitElement {
             }
           </div>
 
-          <div class="players-list">
-            ${this.clients.map(
-              (client) => html`
-                <span class="player-tag">
-                  ${client.username}
-                  ${client.clientID === this.lobbyCreatorClientID
-                    ? html`<span class="host-badge"
-                        >(${translateText("host_modal.host_badge")})</span
-                      >`
-                    : html`
-                        <button
-                          class="remove-player-btn"
-                          @click=${() => this.kickPlayer(client.clientID)}
-                          title="Remove ${client.username}"
-                        >
-                          ×
-                        </button>
-                      `}
-                </span>
-              `,
-            )}
+          <lobby-team-view
+            .gameMode=${this.gameMode}
+            .clients=${this.clients}
+            .lobbyCreatorClientID=${this.lobbyCreatorClientID}
+            .teamCount=${this.teamCount}
+            .onKickPlayer=${(clientID: string) => this.kickPlayer(clientID)}
+          ></lobby-team-view>
         </div>
 
         <div class="start-game-button-container">
