91a88e27e16dc3c7a830b00358f2687f98fb2a10
evanpelle <evanpelle@gmail.com>
2025-11-23 08:12:41 -0800

    remove timeout

--- startup.sh
+++ startup.sh
@@ -2,7 +2,7 @@
 set -e
 # Start supervisord
 if [ "$DOMAIN" = openfront.dev ] && [ "$SUBDOMAIN" != main ]; then
-    exec timeout 18h /usr/bin/supervisord -c /etc/supervisor/conf.d/supervisord.conf
+    exec /usr/bin/supervisord -c /etc/supervisor/conf.d/supervisord.conf
 else
     exec /usr/bin/supervisord -c /etc/supervisor/conf.d/supervisord.conf
 fi
