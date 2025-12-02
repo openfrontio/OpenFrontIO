bcc1232b1e71c18329d498807a25eb11aebf757b
Restart2008 <restart.openfront@gmail.com>
2025-12-01 21:23:33 -0800

    feat: Improve Christmas theme asset loading and availability
    - Remove December month limit for Christmas news modal: The news modal will now always display Christmas content.
    - Update image paths to use Webpack aliases:
        - Modified  to use  for the Santa image.
        - Modified  to use  for the snowflake background.
    - Migrate  import to :  is now imported directly into  to ensure Webpack processes it correctly with asset modules.

--- src/client/NewsModal.ts
+++ src/client/NewsModal.ts
@@ -1,6 +1,7 @@
 import { LitElement, css, html } from "lit";
 import { resolveMarkdown } from "lit-markdown";
 import { customElement, property, query } from "lit/decorators.js";
+import christmas from "../../resources/christmas.md";
 import megaphone from "../../resources/images/Megaphone.svg";
 import version from "../../resources/version.txt";
 import { translateText } from "../client/Utils";
