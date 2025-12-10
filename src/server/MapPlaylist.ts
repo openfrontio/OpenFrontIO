funkyjabroni/OpenFrontUserScript

Replaced GameMode enum with string union.

src/server/MapPlaylist.ts
@@ -71,8 +71,7 @@ export class MapPlaylist {
   public gameConfig(): GameConfig {
     const { map, mode } = this.getNextMap();
 
-    const playerTeams =
-      mode === GameMode.Team ? this.getTeamCount() : undefined;
+    const playerTeams = mode === "Team" ? this.getTeamCount() : undefined;
 
     // Create the default public game config (from your GameManager)
     return {
@@ -83,7 +82,7 @@ export class MapPlaylist {
       infiniteGold: false,
       infiniteTroops: false,
       instantBuild: false,
-      disableNPCs: mode === GameMode.Team,
+      disableNPCs: mode === "Team",
       gameMode: mode,
       playerTeams,
       bots: 400,
@@ -126,13 +125,13 @@ export class MapPlaylist {
 
     this.mapsPlaylist = [];
     for (let i = 0; i < maps.length; i++) {
-      if (!this.addNextMap(this.mapsPlaylist, ffa1, GameMode.FFA)) {
+      if (!this.addNextMap(this.mapsPlaylist, ffa1, "Free For All")) {
         return false;
       }
-      if (!this.addNextMap(this.mapsPlaylist, ffa2, GameMode.FFA)) {
+      if (!this.addNextMap(this.mapsPlaylist, ffa2, "Free For All")) {
         return false;
       }
-      if (!this.addNextMap(this.mapsPlaylist, team, GameMode.Team)) {
+      if (!this.addNextMap(this.mapsPlaylist, team, "Team")) {
         return false;
       }
     }

openfrontio/OpenFrontIO

src/server/MapPlaylist.ts
@@ -78,13 +78,12 @@ export class MapPlaylist {
   public gameConfig(): GameConfig {
     const { map, mode } = this.getNextMap();
 
-    const playerTeams =
-      mode === GameMode.Team ? this.getTeamCount() : undefined;
+    const playerTeams = mode === "Team" ? this.getTeamCount() : undefined;
 
     // Create the default public game config (from your GameManager)
     return {
-      donateGold: mode === GameMode.Team,
-      donateTroops: mode === GameMode.Team,
+      donateGold: mode === "Team",
+      donateTroops: mode === "Team",
       gameMap: map,
       maxPlayers: config.lobbyMaxPlayers(map, mode, playerTeams),
       gameType: GameType.Public,
@@ -95,7 +94,7 @@ export class MapPlaylist {
       maxTimerValue: undefined,
       instantBuild: false,
       randomSpawn: false,
-      disableNPCs: mode === GameMode.Team && playerTeams !== HumansVsNations,
+      disableNPCs: mode === "Team" && playerTeams !== HumansVsNations,
       gameMode: mode,
       playerTeams,
       bots: 400,
@@ -140,23 +139,23 @@ export class MapPlaylist {
 
     this.mapsPlaylist = [];
     for (let i = 0; i < maps.length; i++) {
-      if (!this.addNextMap(this.mapsPlaylist, ffa1, GameMode.FFA)) {
+      if (!this.addNextMap(this.mapsPlaylist, ffa1, "Free For All")) {
         return false;
       }
       if (!this.disableTeams) {
-        if (!this.addNextMap(this.mapsPlaylist, team1, GameMode.Team)) {
+        if (!this.addNextMap(this.mapsPlaylist, team1, "Team")) {
           return false;
         }
       }
-      if (!this.addNextMap(this.mapsPlaylist, ffa2, GameMode.FFA)) {
+      if (!this.addNextMap(this.mapsPlaylist, ffa2, "Free For All")) {
         return false;
       }
       if (!this.disableTeams) {
-        if (!this.addNextMap(this.mapsPlaylist, team2, GameMode.Team)) {
+        if (!this.addNextMap(this.mapsPlaylist, team2, "Team")) {
           return false;
         }
       }
-      if (!this.addNextMap(this.mapsPlaylist, ffa3, GameMode.FFA)) {
+      if (!this.addNextMap(this.mapsPlaylist, ffa3, "Free For All")) {
         return false;
       }
     }