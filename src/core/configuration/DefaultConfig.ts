a7b8a1292d2695463d88e9e14ed624bb6675ee41
evanpelle <evanpelle@gmail.com>
2025-12-17 08:52:54 -0800

    reduce train spawn rate

--- src/core/configuration/DefaultConfig.ts
+++ src/core/configuration/DefaultConfig.ts
@@ -365,7 +365,7 @@ export class DefaultConfig implements Config {
   trainSpawnRate(numPlayerFactories: number): number {
     // hyperbolic decay, midpoint at 10 factories
     // expected number of trains = numPlayerFactories  / trainSpawnRate(numPlayerFactories)
-    return (numPlayerFactories + 10) * 18;
+    return (numPlayerFactories + 10) * 20;
   }
   trainGold(rel: "self" | "team" | "ally" | "other"): Gold {
     switch (rel) {
