f532dab704e1d43d5114d3bca50364e965d62fe1
DevelopingTom <tom@rouillard.org>
2025-12-19 04:41:29 +0100

    Add end of game report window (#2598)
    Resolves #1664
    
    ## Description:
    
    Add a game ranking window, accessible through the player game history:
    <img width="508" height="140" alt="image"
    src="https://github.com/user-attachments/assets/51a628d9-628d-44c3-9776-d9b359b94e65"
    />
    
    There is a lot of data players could be ranked with.
    Three main ranking categories  with their own sub-categories:
    
    <img width="371" height="264" alt="image"
    src="https://github.com/user-attachments/assets/8b3b7c53-c52f-4b96-8039-23180c9181cf"
    />
    
    ### Duration:
    Rank players according to their survival time
    
    <img width="284" height="281" alt="image"
    src="https://github.com/user-attachments/assets/6dfa0d11-7f5b-4f4f-81f8-f31e24ade6bf"
    />
    
    
    ### War:
    #### Conquests:
    Number of conquered players and bots
    
    #### Bombs:
    Show all bomb launched by each players. Can be sorted with each
    category.
    <img width="289" height="193" alt="image"
    src="https://github.com/user-attachments/assets/fc0f9663-9a50-4098-b5c6-f434354accff"
    />
    
    ### Economy:
    Show all gold earned by each players with trade, conquests, pirate or
    total:
    
    <img width="276" height="195" alt="image"
    src="https://github.com/user-attachments/assets/a925249d-b2d2-4c61-92a5-4dbf5922b32b"
    />
    
    
    ### Responsiveness:
    
    
    ![ranking_showcase_resize](https://github.com/user-attachments/assets/5316d7f4-803f-4223-b834-783040226b7d)
    
    
    ## Please complete the following:
    
    - [x] I have added screenshots for all UI updates
    - [x] I process any text displayed to the user through translateText()
    and I've added it to the en.json file
    - [x] I have added relevant tests to the test directory
    - [x] I confirm I have thoroughly tested these changes and take full
    responsibility for any bugs introduced
    
    ## Please put your Discord username so you can be contacted if a bug or
    regression is found:
    
    IngloriousTom

---
+++ src/client/components/baseComponents/ranking/RankingHeader.ts
@@ -0,0 +1,93 @@
+import { LitElement, html, nothing } from "lit";
+import { customElement, property } from "lit/decorators.js";
+import { translateText } from "../../../Utils";
+import { RankType } from "./GameInfoRanking";
+
+@customElement("ranking-header")
+export class RankingHeader extends LitElement {
+  @property({ type: String }) rankType = RankType.Lifetime;
+
+  private onSort(type: RankType) {
+    this.dispatchEvent(new CustomEvent("sort", { detail: type }));
+  }
+
+  render() {
+    return html`
+      <li
+        class="text-lg bg-gray-800 font-bold relative pt-2 pb-2 pr-5 pl-5 mb-[5px] rounded-md flex justify-between items-center"
+      >
+        ${this.renderHeaderContent()}
+      </li>
+    `;
+  }
+
+  private renderHeaderContent() {
+    switch (this.rankType) {
+      case RankType.Lifetime:
+        return html`<div class="w-full">
+          ${translateText("game_info_modal.survival_time")}
+        </div>`;
+      case RankType.Conquests:
+        return html`<div class="w-full">
+          ${translateText("game_info_modal.num_of_conquests")}
+        </div>`;
+      case RankType.Atoms:
+      case RankType.Hydros:
+      case RankType.MIRV:
+        return html`
+          <div class="flex justify-between sm:pl-[70px] sm:pr-[70px] w-full">
+            ${this.renderBombHeaderButton(
+              translateText("game_info_modal.atoms"),
+              RankType.Atoms,
+            )}
+            /
+            ${this.renderBombHeaderButton(
+              translateText("game_info_modal.hydros"),
+              RankType.Hydros,
+            )}
+            /
+            ${this.renderBombHeaderButton(
+              translateText("game_info_modal.mirv"),
+              RankType.MIRV,
+            )}
+          </div>
+        `;
+      case RankType.TotalGold:
+        return html`<div class="w-full">
+          ${translateText("game_info_modal.all_gold")}
+        </div>`;
+      case RankType.TradedGold:
+        return html`<div class="w-full">
+          ${translateText("game_info_modal.trade")}
+        </div>`;
+      case RankType.ConqueredGold:
+        return html`<div class="w-full">
+          ${translateText("game_info_modal.conquest_gold")}
+        </div>`;
+      case RankType.StolenGold:
+        return html`<div class="w-full">
+          ${translateText("game_info_modal.stolen_gold")}
+        </div>`;
+      default:
+        console.warn("Unhandled RankType", this.rankType);
+        return null;
+    }
+  }
+
+  private renderBombHeaderButton(label: string, type: RankType) {
+    return html`
+      <button
+        @click=${() => this.onSort(type)}
+        style="${this.rankType === type
+          ? "border-bottom: solid 2px white;"
+          : nothing}"
+      >
+        ${label}
+      </button>
+    `;
+  }
+
+  createRenderRoot() {
+    return this;
+  }
+}
