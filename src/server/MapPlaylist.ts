funkyjabroni/OpenFrontUserScript

Replaced Difficulty enum with string union.

src/server/MapPlaylist.ts
@@ -1,6 +1,5 @@
 import { getServerConfigFromServer } from "../core/configuration/ConfigLoader";
 import {
-  Difficulty,
   Duos,
   GameMapName,
   GameMapType,
@@ -81,7 +80,7 @@ export class MapPlaylist {
       gameMap: map,
       maxPlayers: config.lobbyMaxPlayers(map, mode, playerTeams),
       gameType: GameType.Public,
-      difficulty: Difficulty.Medium,
+      difficulty: "Medium",
       infiniteGold: false,
       infiniteTroops: false,
       instantBuild: false,

openfrontio/OpenFrontIO

src/server/MapPlaylist.ts
@@ -1,6 +1,5 @@
 import { getServerConfigFromServer } from "../core/configuration/ConfigLoader";
 import {
-  Difficulty,
   Duos,
   GameMapName,
   GameMapSize,
@@ -93,7 +92,7 @@ export class MapPlaylist {
       maxPlayers: config.lobbyMaxPlayers(map, mode, playerTeams),
       gameType: GameType.Public,
       gameMapSize: GameMapSize.Normal,
-      difficulty: Difficulty.Easy,
+      difficulty: "Easy",
       infiniteGold: false,
       infiniteTroops: false,
       maxTimerValue: undefined,