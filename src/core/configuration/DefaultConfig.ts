af0b8a8d50f625f11dbc5bdd209ec92c804c2a5d
DevelopingTom <tom@rouillard.org>
2026-01-04 05:04:48 +0100

    Configurable immunity timer (#2763)
    ## Description:
    
    Resolve discussions about stalled PR
    https://github.com/openfrontio/OpenFrontIO/pull/2460
    
    <img width="724" height="348" alt="image"
    src="https://github.com/user-attachments/assets/c2c9fa79-cace-431a-9ca4-b3656612fa9d"
    />
    
    Changes:
    - Added a `Player::canAttackPlayer(other)` function to determine whether
    a player can be attacked.
    - This function is now used in most places where a fight can occur:
        - AttackExecution (land attacks)
        - Naval invasion
        - Warship fight
    - Nukes can't be thrown during the truce
    - Immunity only affect human players. Nations and bot will fight as
    usual, and can be fought against.
    - The immunity timer uses minutes in the modal window.
    
    UI:
    
    - The immunity phase is displayed with a timer bar at the top. This is
    from the original PR, to be discussed if it's not deemed visible enough:
    
    <img width="632" height="215" alt="image"
    src="https://github.com/user-attachments/assets/f5ab9aa0-bd4f-4503-b8d6-b40b121fba65"
    />
    
    
    ## Please complete the following:
    
    - [x] I have added screenshots for all UI updates
    - [x] I process any text displayed to the user through translateText()
    and I've added it to the en.json file
    - [x] I have added relevant tests to the test directory
    - [x] I confirm I have thoroughly tested these changes and take full
    responsibility for any bugs introduced
    
    ## Please put your Discord username so you can be contacted if a bug or
    regression is found:
    
    IngloriousTom
    
    ---------
    
    Co-authored-by: newyearnewphil <git@nynp.dev>

--- src/core/configuration/DefaultConfig.ts
+++ src/core/configuration/DefaultConfig.ts
@@ -246,7 +246,7 @@ export class DefaultConfig implements Config {
     return 30 * 10; // 30 seconds
   }
   spawnImmunityDuration(): Tick {
-    return 5 * 10;
+    return this._gameConfig.spawnImmunityDuration ?? 5 * 10; // default to 5 seconds
   }
 
   gameConfig(): GameConfig {
