a4a6f2975cd2787ecb13a8872d7ae1f0276e0a09
Ryan Barlow <ryanbarlow123@me.com>
2026-01-08 23:43:28 +0000

    Localise the Back button

--- src/client/StatsModal.ts
+++ src/client/StatsModal.ts
@@ -308,7 +308,7 @@ export class StatsModal extends BaseModal {
             <button
               @click=${this.close}
               class="group flex items-center justify-center w-10 h-10 rounded-full bg-white/5 hover:bg-white/10 transition-all border border-white/10"
-              aria-label="Back"
+              aria-label=${translateText("common.close")}
             >
               <svg
                 xmlns="http://www.w3.org/2000/svg"
