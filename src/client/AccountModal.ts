07a9d4423b954fbaad5514d9b1e6bcdfba4ab7f6
Ryan Barlow <ryanbarlow123@me.com>
2026-01-10 22:40:40 +0000

    remove email button

--- src/client/AccountModal.ts
+++ src/client/AccountModal.ts
@@ -454,25 +454,6 @@ export class AccountModal extends BaseModal {
                   )}"
                   required
                 />
-                <div
-                  class="absolute right-4 top-1/2 -translate-y-1/2 text-white/20"
-                >
-                  <svg
-                    xmlns="http://www.w3.org/2000/svg"
-                    class="w-5 h-5"
-                    viewBox="0 0 24 24"
-                    fill="none"
-                    stroke="currentColor"
-                    stroke-width="2"
-                    stroke-linecap="round"
-                    stroke-linejoin="round"
-                  >
-                    <path
-                      d="M4 4h16c1.1 0 2 .9 2 2v12c0 1.1-.9 2-2 2H4c-1.1 0-2-.9-2-2V6c0-1.1.9-2 2-2z"
-                    ></path>
-                    <polyline points="22,6 12,13 2,6"></polyline>
-                  </svg>
-                </div>
               </div>
               <button
                 @click="${this.handleSubmit}"
