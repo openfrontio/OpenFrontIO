de3c97c303aab32c261537b2cb802edc3f2336c4
Ryan Barlow <ryan.barlow@ucb.com>
2026-01-23 11:51:00 +0000

    reuse sockets

--- src/client/Main.ts
+++ src/client/Main.ts
@@ -200,6 +200,7 @@ declare global {
     "join-lobby": CustomEvent<JoinLobbyEvent>;
     "kick-player": CustomEvent;
     "join-changed": CustomEvent;
+    "lobby-info": CustomEvent<GameInfo>;
   }
 }
 
