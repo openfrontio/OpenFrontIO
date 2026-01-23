379e3ecacb8eb5c1275f05253b997338fe3e65cc
Ryan Barlow <ryan.barlow@ucb.com>
2026-01-23 15:47:54 +0000

    prettier

--- src/client/Main.ts
+++ src/client/Main.ts
@@ -243,8 +243,9 @@ class Client {
   private serverConfigPrefetch: Promise<
     Awaited<ReturnType<typeof getServerConfigFromClient>>
   > | null = null;
-  private cosmeticsPromise: Promise<Awaited<ReturnType<typeof fetchCosmetics>>> | null =
-    null;
+  private cosmeticsPromise: Promise<
+    Awaited<ReturnType<typeof fetchCosmetics>>
+  > | null = null;
 
   constructor() {}
 
