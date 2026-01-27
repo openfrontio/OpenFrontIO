e7074a69bf1aede4d396dcc12baeb848d60cba5f
Ryan Barlow <ryanbarlow123@me.com>
2026-01-27 18:03:08 +0000

    fetchCosmetics() is called fresh each join

--- src/client/Main.ts
+++ src/client/Main.ts
@@ -242,9 +242,6 @@ class Client {
   private serverConfigPrefetch: Promise<
     Awaited<ReturnType<typeof getServerConfigFromClient>>
   > | null = null;
-  private cosmeticsPromise: Promise<
-    Awaited<ReturnType<typeof fetchCosmetics>>
-  > | null = null;
 
   constructor() {
     this.turnstileManager = new TurnstileManager(() =>
@@ -263,7 +260,6 @@ class Client {
       }
     });
     this.serverConfigPrefetch = configPrefetch;
-    this.cosmeticsPromise = fetchCosmetics();
     // Prefetch turnstile token so it is available when the user joins a lobby.
     this.turnstileManager.warmup();
 
@@ -831,14 +827,13 @@ class Client {
       this.joinModal?.close();
       this.joinPublicModal?.open(lobby.gameID, lobby.publicLobbyInfo);
     }
-    const cosmeticsPromise = this.cosmeticsPromise ?? fetchCosmetics();
     let config: Awaited<ReturnType<typeof getServerConfigFromClient>>;
     let cosmetics: Awaited<ReturnType<typeof fetchCosmetics>>;
     let turnstileToken: string | null;
     try {
       [config, cosmetics, turnstileToken] = await Promise.all([
         this.getServerConfigPrefetched(),
-        cosmeticsPromise,
+        fetchCosmetics(),
         this.turnstileManager.getTokenForJoin(lobby.gameStartInfo),
       ]);
     } catch (error) {
@@ -847,9 +842,6 @@ class Client {
       }
       if (joinAttemptId === this.joinAttemptId) {
         this.isJoiningLobby = false;
-        if (this.cosmeticsPromise === cosmeticsPromise) {
-          this.cosmeticsPromise = null;
-        }
         this.joinModal?.close();
         this.joinPublicModal?.closeWithoutLeaving();
       }
@@ -861,9 +853,6 @@ class Client {
       joinAbortController.signal.aborted
     ) {
       return;
-    }
-    if (this.cosmeticsPromise === cosmeticsPromise && cosmetics === null) {
-      this.cosmeticsPromise = null;
     }
     const pattern = this.userSettings.getSelectedPatternName(cosmetics);
 
