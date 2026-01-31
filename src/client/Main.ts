2a09972bbd34a714e96f9b9665b45ff8a04cf884
Ryan Barlow <ryanbarlow123@me.com>
2026-01-31 23:03:24 +0000

    revert fusetag

--- src/client/Main.ts
+++ src/client/Main.ts
@@ -205,10 +205,6 @@ declare global {
       BOLT_AD_CLICKED: string;
       SHOW_HIDDEN_CONTAINER: string;
     };
-    fusetag?: {
-      que: Array<() => void>;
-      pageInit: (config: { blockingFuseIds: string[] }) => void;
-    };
     showPage?: (pageId: string) => void;
   }
 
