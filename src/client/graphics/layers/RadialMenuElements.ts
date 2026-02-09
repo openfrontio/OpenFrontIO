20f0f3e00497b14580c215357687e3ab1b09de18
VariableVince <24507472+VariableVince@users.noreply.github.com>
2026-02-09 19:58:06 +0100

    Prettier

--- src/client/graphics/layers/RadialMenuElements.ts
+++ src/client/graphics/layers/RadialMenuElements.ts
@@ -629,7 +629,9 @@ export const rootMenuElement: MenuElement = {
       ...(isOwnTerritory
         ? [deleteUnitElement, allyRequestElement, buildMenuElement]
         : [
-            isAllied && !isDisconnectedTarget(params) ? allyBreakElement : boatMenuElement,
+            isAllied && !isDisconnectedTarget(params)
+              ? allyBreakElement
+              : boatMenuElement,
             allyRequestElement,
             isFriendlyTarget(params) && !isDisconnectedTarget(params)
               ? donateGoldRadialElement
