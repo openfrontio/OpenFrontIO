40a7b49b8953911c5dd158bdabe21e6f60958f9d
evanpelle <evanpelle@gmail.com>
2026-03-03 13:34:45 -0800

    Bugfix: Update PlayerGameSchema.map from GameMapType enum to string
    Same issue as the users/@me response: a user played a map that doesn't exist on production

--- src/core/ApiSchemas.ts
+++ src/core/ApiSchemas.ts
@@ -1,7 +1,7 @@
 import { z } from "zod";
 import { base64urlToUuid } from "./Base64";
 import { BigIntStringSchema, PlayerStatsSchema } from "./StatsSchemas";
-import { Difficulty, GameMapType, GameMode, GameType } from "./game/Game";
+import { Difficulty, GameMode, GameType } from "./game/Game";
 
 export const RefreshResponseSchema = z.object({
   token: z.string(),
@@ -99,7 +99,7 @@ export const PlayerGameSchema = z.object({
   start: z.iso.datetime(),
   mode: z.enum(GameMode),
   type: z.enum(GameType),
-  map: z.enum(GameMapType),
+  map: z.string(),
   difficulty: z.enum(Difficulty),
   clientId: z.string().optional(),
 });
