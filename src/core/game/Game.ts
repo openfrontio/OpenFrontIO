3c368ef371ed0015e0a0bded2a61955c9413210e
RickD004 <realtacoco@gmail.com>
2026-03-03 17:22:25 -0600

    Update Game.ts
    added arctic to Game.ts

--- src/core/game/Game.ts
+++ src/core/game/Game.ts
@@ -136,6 +136,7 @@ export enum GameMapType {
   Hawaii = "Hawaii",
   Alps = "Alps",
   NileDelta = "Nile Delta",
+  Arctic = "Arctic",
 }
 
 export type GameMapName = keyof typeof GameMapType;
@@ -189,6 +190,7 @@ export const mapCategories: Record<string, GameMapType[]> = {
     GameMapType.Hawaii,
     GameMapType.Alps,
     GameMapType.NileDelta,
+    GameMapType.Arctic,
   ],
   fantasy: [
     GameMapType.Pangaea,
