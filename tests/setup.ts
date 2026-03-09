05bb572075df31106f3e09936861459815973c84
Mitch Mullvain <experienceit12@gmail.com>
2026-03-09 00:55:26 -0500

    Update setup.ts

--- tests/setup.ts
+++ tests/setup.ts
@@ -1,5 +1,2 @@
 // Add global mocks or configuration here if needed
-Object.defineProperty(globalThis, "localStorage", {
-  value: window.localStorage,
-  configurable: true,
-});
+import "vitest-canvas-mock";
