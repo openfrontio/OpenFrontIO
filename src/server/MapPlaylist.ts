b82c3c35ae52312923f094769df6a5eddb8e05cd
flucci14 <tn340975@gmail.com>
2026-03-18 15:41:30 +0100

    Add Moldova Map

--- src/server/MapPlaylist.ts
+++ src/server/MapPlaylist.ts
@@ -82,6 +82,7 @@ const frequency: Partial<Record<GameMapName, number>> = {
   Arctic: 6,
   SanFrancisco: 3,
   Aegean: 6,
+  Moldova: 8,
 };
 
 const TEAM_WEIGHTS: { config: TeamCountConfig; weight: number }[] = [
