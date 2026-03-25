f944ed25fef1eebdd82439095b264d7a0079d6f8
Ricky G.P. <realtacoco@gmail.com>
2026-03-25 00:22:46 -0600

    adds India map

--- src/core/game/Game.ts
+++ src/core/game/Game.ts
@@ -143,6 +143,7 @@ export enum GameMapType {
   Arctic = "Arctic",
   SanFrancisco = "San Francisco",
   Aegean = "Aegean",
+  India = "India",
 }
 
 export type GameMapName = keyof typeof GameMapType;
@@ -194,6 +195,7 @@ export const mapCategories: Record<string, GameMapType[]> = {
     GameMapType.Arctic,
     GameMapType.SanFrancisco,
     GameMapType.Aegean,
+    GameMapType.India,
   ],
   fantasy: [
     GameMapType.Pangaea,
