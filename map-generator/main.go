2d4a1569fdabd32b8a4622f877d847d4dda086db
Patrick Plays Badly <patrickbadly@gmail.com>
2026-04-02 21:07:34 -0400

    Update main.go
    add map dyslexdria

--- map-generator/main.go
+++ map-generator/main.go
@@ -85,6 +85,7 @@ var maps = []struct {
 	{Name: "aegean"},
 	{Name: "milkyway"},
 	{Name: "mediterranean"},
+  {name: "dyslexdria"},
 	{Name: "big_plains", IsTest: true},
 	{Name: "half_land_half_ocean", IsTest: true},
 	{Name: "ocean_and_land", IsTest: true},
