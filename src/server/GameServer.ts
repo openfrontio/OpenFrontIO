061d628bbd62ca47f44106509e1093c0d46cac4d
babyboucher <48159308+babyboucher@users.noreply.github.com>
2026-04-05 04:24:07 -0500

    Add PostGame to GamePhase

--- src/server/GameServer.ts
+++ src/server/GameServer.ts
@@ -31,6 +31,7 @@ export enum GamePhase {
   Lobby = "LOBBY",
   Active = "ACTIVE",
   Finished = "FINISHED",
+  PostGame = "POSTGAME",
 }
 
 const KICK_REASON_DUPLICATE_SESSION = "kick_reason.duplicate_session";
@@ -859,6 +860,8 @@ export class GameServer {
             gameID: this.id,
           });
           return GamePhase.Finished;
+        } else if (this.winner) {
+          return GamePhase.PostGame;
         } else {
           return GamePhase.Active;
         }
@@ -882,6 +885,10 @@ export class GameServer {
     const warmupOver = now > this.startsAt! + 30 * 1000;
     if (noActive && warmupOver && noRecentPings) {
       return GamePhase.Finished;
+    }
+
+    if (this.winner) {
+      return GamePhase.PostGame;
     }
 
     return GamePhase.Active;
