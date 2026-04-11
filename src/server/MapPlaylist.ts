a47dbb49bb5cd9deef8100c1b74eaa06870be411
Patrick Plays Badly <patrickbadly@gmail.com>
2026-04-10 20:54:12 -0400

    Update MapPlaylist.ts
    Added map luna

--- src/server/MapPlaylist.ts
+++ src/server/MapPlaylist.ts
@@ -89,6 +89,7 @@ const frequency: Partial<Record<GameMapName, number>> = {
   Dyslexdria: 8,
   GreatLakes: 6,
   StraitOfMalacca: 4,
+  Luna 6,
 };
 
 const TEAM_WEIGHTS: { config: TeamCountConfig; weight: number }[] = [
