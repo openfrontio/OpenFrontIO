d3f9f925a6e47710f5b7055df58809b3560174a6
Patrick Plays Badly <patrickbadly@gmail.com>
2026-04-12 20:11:44 -0400

    Update main.go
    added conakry

--- map-generator/main.go
+++ map-generator/main.go
@@ -89,6 +89,7 @@ var maps = []struct {
 	{Name: "greatlakes"},
   {Name: "dyslexdria"},
   {Name: "luna"},
+  {Name: "conakry"},
 	{Name: "big_plains", IsTest: true},
 	{Name: "half_land_half_ocean", IsTest: true},
 	{Name: "ocean_and_land", IsTest: true},
