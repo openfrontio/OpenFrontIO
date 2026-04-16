383c44dc7fddd2dc67323bdd3a09da52110d67a7
FloPinguin <25036848+FloPinguin@users.noreply.github.com>
2026-04-17 01:50:57 +0200

    Fix warship bug

--- src/core/execution/WarshipExecution.ts
+++ src/core/execution/WarshipExecution.ts
@@ -242,13 +242,17 @@ export class WarshipExecution implements Execution {
     // Get warship's water component for connectivity check
     const warshipComponent = this.mg.getWaterComponent(this.warship.tile());
 
+    const patrolTile = this.warship.patrolTile();
+    if (patrolTile === undefined) {
+      return undefined;
+    }
+
     while (expandCount < 3) {
+      const halfRange = Math.floor(warshipPatrolRange / 2);
       const x =
-        this.mg.x(this.warship.patrolTile()!) +
-        this.random.nextInt(-warshipPatrolRange / 2, warshipPatrolRange / 2);
+        this.mg.x(patrolTile) + this.random.nextInt(-halfRange, halfRange);
       const y =
-        this.mg.y(this.warship.patrolTile()!) +
-        this.random.nextInt(-warshipPatrolRange / 2, warshipPatrolRange / 2);
+        this.mg.y(patrolTile) + this.random.nextInt(-halfRange, halfRange);
       if (!this.mg.isValidCoord(x, y)) {
         continue;
       }
