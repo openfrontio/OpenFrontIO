a9d767b530cc34bd1ef39be06b9c418bab72114f
babyboucher <48159308+babyboucher@users.noreply.github.com>
2026-05-07 14:30:42 -0500

    Replace hardcoded defaults

--- src/client/UserSettingModal.ts
+++ src/client/UserSettingModal.ts
@@ -386,7 +386,7 @@ export class UserSettingModal extends BaseModal {
         action="toggleView"
         label=${translateText("user_setting.toggle_view")}
         description=${translateText("user_setting.toggle_view_desc")}
-        defaultKey="Space"
+        defaultKey=${this.defaultKeybinds.toggleView}
         .value=${this.getKeyValue("toggleView")}
         .display=${this.getKeyChar("toggleView")}
         @change=${this.handleKeybindChange}
@@ -412,7 +412,7 @@ export class UserSettingModal extends BaseModal {
         action="buildCity"
         label=${translateText("user_setting.build_city")}
         description=${translateText("user_setting.build_city_desc")}
-        defaultKey="Digit1"
+        defaultKey=${this.defaultKeybinds.buildCity}
         .value=${this.getKeyValue("buildCity")}
         .display=${this.getKeyChar("buildCity")}
         @change=${this.handleKeybindChange}
@@ -422,7 +422,7 @@ export class UserSettingModal extends BaseModal {
         action="buildFactory"
         label=${translateText("user_setting.build_factory")}
         description=${translateText("user_setting.build_factory_desc")}
-        defaultKey="Digit2"
+        defaultKey=${this.defaultKeybinds.buildFactory}
         .value=${this.getKeyValue("buildFactory")}
         .display=${this.getKeyChar("buildFactory")}
         @change=${this.handleKeybindChange}
@@ -432,7 +432,7 @@ export class UserSettingModal extends BaseModal {
         action="buildPort"
         label=${translateText("user_setting.build_port")}
         description=${translateText("user_setting.build_port_desc")}
-        defaultKey="Digit3"
+        defaultKey=${this.defaultKeybinds.buildPort}
         .value=${this.getKeyValue("buildPort")}
         .display=${this.getKeyChar("buildPort")}
         @change=${this.handleKeybindChange}
@@ -442,7 +442,7 @@ export class UserSettingModal extends BaseModal {
         action="buildDefensePost"
         label=${translateText("user_setting.build_defense_post")}
         description=${translateText("user_setting.build_defense_post_desc")}
-        defaultKey="Digit4"
+        defaultKey=${this.defaultKeybinds.buildDefensePost}
         .value=${this.getKeyValue("buildDefensePost")}
         .display=${this.getKeyChar("buildDefensePost")}
         @change=${this.handleKeybindChange}
@@ -452,7 +452,7 @@ export class UserSettingModal extends BaseModal {
         action="buildMissileSilo"
         label=${translateText("user_setting.build_missile_silo")}
         description=${translateText("user_setting.build_missile_silo_desc")}
-        defaultKey="Digit5"
+        defaultKey=${this.defaultKeybinds.buildMissileSilo}
         .value=${this.getKeyValue("buildMissileSilo")}
         .display=${this.getKeyChar("buildMissileSilo")}
         @change=${this.handleKeybindChange}
@@ -462,7 +462,7 @@ export class UserSettingModal extends BaseModal {
         action="buildSamLauncher"
         label=${translateText("user_setting.build_sam_launcher")}
         description=${translateText("user_setting.build_sam_launcher_desc")}
-        defaultKey="Digit6"
+        defaultKey=${this.defaultKeybinds.buildSamLauncher}
         .value=${this.getKeyValue("buildSamLauncher")}
         .display=${this.getKeyChar("buildSamLauncher")}
         @change=${this.handleKeybindChange}
@@ -472,7 +472,7 @@ export class UserSettingModal extends BaseModal {
         action="buildWarship"
         label=${translateText("user_setting.build_warship")}
         description=${translateText("user_setting.build_warship_desc")}
-        defaultKey="Digit7"
+        defaultKey=${this.defaultKeybinds.buildWarship}
         .value=${this.getKeyValue("buildWarship")}
         .display=${this.getKeyChar("buildWarship")}
         @change=${this.handleKeybindChange}
@@ -482,7 +482,7 @@ export class UserSettingModal extends BaseModal {
         action="buildAtomBomb"
         label=${translateText("user_setting.build_atom_bomb")}
         description=${translateText("user_setting.build_atom_bomb_desc")}
-        defaultKey="Digit8"
+        defaultKey=${this.defaultKeybinds.buildAtomBomb}
         .value=${this.getKeyValue("buildAtomBomb")}
         .display=${this.getKeyChar("buildAtomBomb")}
         @change=${this.handleKeybindChange}
@@ -492,7 +492,7 @@ export class UserSettingModal extends BaseModal {
         action="buildHydrogenBomb"
         label=${translateText("user_setting.build_hydrogen_bomb")}
         description=${translateText("user_setting.build_hydrogen_bomb_desc")}
-        defaultKey="Digit9"
+        defaultKey=${this.defaultKeybinds.buildHydrogenBomb}
         .value=${this.getKeyValue("buildHydrogenBomb")}
         .display=${this.getKeyChar("buildHydrogenBomb")}
         @change=${this.handleKeybindChange}
@@ -502,7 +502,7 @@ export class UserSettingModal extends BaseModal {
         action="buildMIRV"
         label=${translateText("user_setting.build_mirv")}
         description=${translateText("user_setting.build_mirv_desc")}
-        defaultKey="Digit0"
+        defaultKey=${this.defaultKeybinds.buildMIRV}
         .value=${this.getKeyValue("buildMIRV")}
         .display=${this.getKeyChar("buildMIRV")}
         @change=${this.handleKeybindChange}
@@ -576,7 +576,7 @@ export class UserSettingModal extends BaseModal {
         description=${translateText("user_setting.attack_ratio_down_desc", {
           amount: this.userSettings.attackRatioIncrement(),
         })}
-        defaultKey="KeyT"
+        defaultKey=${this.defaultKeybinds.attackRatioDown}
         .value=${this.getKeyValue("attackRatioDown")}
         .display=${this.getKeyChar("attackRatioDown")}
         @change=${this.handleKeybindChange}
@@ -588,7 +588,7 @@ export class UserSettingModal extends BaseModal {
         description=${translateText("user_setting.attack_ratio_up_desc", {
           amount: this.userSettings.attackRatioIncrement(),
         })}
-        defaultKey="KeyY"
+        defaultKey=${this.defaultKeybinds.attackRatioUp}
         .value=${this.getKeyValue("attackRatioUp")}
         .display=${this.getKeyChar("attackRatioUp")}
         @change=${this.handleKeybindChange}
@@ -604,7 +604,7 @@ export class UserSettingModal extends BaseModal {
         action="boatAttack"
         label=${translateText("user_setting.boat_attack")}
         description=${translateText("user_setting.boat_attack_desc")}
-        defaultKey="KeyB"
+        defaultKey=${this.defaultKeybinds.boatAttack}
         .value=${this.getKeyValue("boatAttack")}
         .display=${this.getKeyChar("boatAttack")}
         @change=${this.handleKeybindChange}
@@ -614,7 +614,7 @@ export class UserSettingModal extends BaseModal {
         action="groundAttack"
         label=${translateText("user_setting.ground_attack")}
         description=${translateText("user_setting.ground_attack_desc")}
-        defaultKey="KeyG"
+        defaultKey=${this.defaultKeybinds.groundAttack}
         .value=${this.getKeyValue("groundAttack")}
         .display=${this.getKeyChar("groundAttack")}
         @change=${this.handleKeybindChange}
@@ -650,7 +650,7 @@ export class UserSettingModal extends BaseModal {
         action="requestAlliance"
         label=${translateText("user_setting.request_alliance")}
         description=${translateText("user_setting.request_alliance_desc")}
-        defaultKey="KeyK"
+        defaultKey=${this.defaultKeybinds.requestAlliance}
         .value=${this.getKeyValue("requestAlliance")}
         .display=${this.getKeyChar("requestAlliance")}
         @change=${this.handleKeybindChange}
@@ -660,7 +660,7 @@ export class UserSettingModal extends BaseModal {
         action="breakAlliance"
         label=${translateText("user_setting.break_alliance")}
         description=${translateText("user_setting.break_alliance_desc")}
-        defaultKey="KeyL"
+        defaultKey=${this.defaultKeybinds.breakAlliance}
         .value=${this.getKeyValue("breakAlliance")}
         .display=${this.getKeyChar("breakAlliance")}
         @change=${this.handleKeybindChange}
@@ -676,7 +676,7 @@ export class UserSettingModal extends BaseModal {
         action="zoomOut"
         label=${translateText("user_setting.zoom_out")}
         description=${translateText("user_setting.zoom_out_desc")}
-        defaultKey="KeyQ"
+        defaultKey=${this.defaultKeybinds.zoomOut}
         .value=${this.getKeyValue("zoomOut")}
         .display=${this.getKeyChar("zoomOut")}
         @change=${this.handleKeybindChange}
@@ -686,7 +686,7 @@ export class UserSettingModal extends BaseModal {
         action="zoomIn"
         label=${translateText("user_setting.zoom_in")}
         description=${translateText("user_setting.zoom_in_desc")}
-        defaultKey="KeyE"
+        defaultKey=${this.defaultKeybinds.zoomIn}
         .value=${this.getKeyValue("zoomIn")}
         .display=${this.getKeyChar("zoomIn")}
         @change=${this.handleKeybindChange}
@@ -702,7 +702,7 @@ export class UserSettingModal extends BaseModal {
         action="centerCamera"
         label=${translateText("user_setting.center_camera")}
         description=${translateText("user_setting.center_camera_desc")}
-        defaultKey="KeyC"
+        defaultKey=${this.defaultKeybinds.centerCamera}
         .value=${this.getKeyValue("centerCamera")}
         .display=${this.getKeyChar("centerCamera")}
         @change=${this.handleKeybindChange}
@@ -712,7 +712,7 @@ export class UserSettingModal extends BaseModal {
         action="moveUp"
         label=${translateText("user_setting.move_up")}
         description=${translateText("user_setting.move_up_desc")}
-        defaultKey="KeyW"
+        defaultKey=${this.defaultKeybinds.moveUp}
         .value=${this.getKeyValue("moveUp")}
         .display=${this.getKeyChar("moveUp")}
         @change=${this.handleKeybindChange}
@@ -722,7 +722,7 @@ export class UserSettingModal extends BaseModal {
         action="moveLeft"
         label=${translateText("user_setting.move_left")}
         description=${translateText("user_setting.move_left_desc")}
-        defaultKey="KeyA"
+        defaultKey=${this.defaultKeybinds.moveLeft}
         .value=${this.getKeyValue("moveLeft")}
         .display=${this.getKeyChar("moveLeft")}
         @change=${this.handleKeybindChange}
@@ -732,7 +732,7 @@ export class UserSettingModal extends BaseModal {
         action="moveDown"
         label=${translateText("user_setting.move_down")}
         description=${translateText("user_setting.move_down_desc")}
-        defaultKey="KeyS"
+        defaultKey=${this.defaultKeybinds.moveDown}
         .value=${this.getKeyValue("moveDown")}
         .display=${this.getKeyChar("moveDown")}
         @change=${this.handleKeybindChange}
@@ -742,7 +742,7 @@ export class UserSettingModal extends BaseModal {
         action="moveRight"
         label=${translateText("user_setting.move_right")}
         description=${translateText("user_setting.move_right_desc")}
-        defaultKey="KeyD"
+        defaultKey=${this.defaultKeybinds.moveRight}
         .value=${this.getKeyValue("moveRight")}
         .display=${this.getKeyChar("moveRight")}
         @change=${this.handleKeybindChange}
