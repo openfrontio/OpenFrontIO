03d7448a39dd9dd02af2b542886171c6f814ef74
FloPinguin <25036848+FloPinguin@users.noreply.github.com>
2026-05-11 20:41:14 +0200

    Add additionalNations pool for map manifests

--- src/core/GameRunner.ts
+++ src/core/GameRunner.ts
@@ -57,6 +57,7 @@ export async function createGameRunner(
   const nations = createNationsForGame(
     gameStart,
     gameMap.nations,
+    gameMap.additionalNations,
     humans.length,
     random,
   );
