d8e52eb293d39da8cee559a6fcd3d0bd04997be9
TKTK123456 <103334266+TKTK123456@users.noreply.github.com>
2026-06-11 21:00:19 -0400

    Added  numpad compatibility to buildkeybinds

--- src/client/InputHandler.ts
+++ src/client/InputHandler.ts
@@ -314,7 +314,7 @@ export class InputHandler {
         this.activeKeys.has("AltLeft") || this.activeKeys.has("AltRight"),
     );
 
-    const buildKeybinds = [
+    let buildKeybinds: string[] = [
       "buildCity",
       "buildFactory",
       "buildPort",
@@ -326,9 +326,14 @@ export class InputHandler {
       "buildWarship",
       "buildMIRV",
     ];
+    buildKeybinds = buildKeybinds.map((i: string): string => {
+      return this.keybinds[i]
+    })
+    buildKeybinds.push(...["Numpad0", "Numpad1", "Numpad2", "Numpad3", "Numpad4", "Numpad5", "Numpad6", "Numpad7", "Numpad8", "Numpad9"])
     for (const i of buildKeybinds) {
+      console.log(i)
       this.addKeybindAndEvent(
-        this.keybinds[i],
+        i,
         (e: KeyboardEvent) => {
           const matchedBuild = this.resolveBuildKeybind(e.code, e.shiftKey);
 
