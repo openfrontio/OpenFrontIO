ca4525ad1bf5a93d6df5a672cda4479baf778ca2
crunchybbb <crunchybbb2@gmail.com>
2026-09-11 20:17:56 -0700

    adds channel island map

--- src/core/game/Maps.gen.ts
+++ src/core/game/Maps.gen.ts
@@ -31,6 +31,7 @@ export enum GameMapType {
   CaspianSea = "Caspian Sea", // map-generator/assets/maps/caspiansea/info.json
   Caucasus = "Caucasus", // map-generator/assets/maps/caucasus/info.json
   CentralAmerica = "Central America", // map-generator/assets/maps/centralamerica/info.json
+  ChannelIslands = "Channel Islands", // map-generator/assets/maps/channelislands/info.json
   China = "China", // map-generator/assets/maps/china/info.json
   ChoppingBlock = "Chopping Block", // map-generator/assets/maps/choppingblock/info.json
   ClearwaterLakes = "Clearwater Lakes", // map-generator/assets/maps/clearwaterlakes/info.json
@@ -579,6 +580,17 @@ export const maps: readonly MapInfo[] = [
     specialFrequency: -1,
     defaultNationCount: 22,
     themes: ["north_america", "south_america"],
+  },
+  {
+    id: "ChannelIslands",
+    type: GameMapType.ChannelIslands,
+    translationKey: "map.channelislands",
+    categories: ["new", "north_america"],
+    multiplayerFrequency: 7,
+    ffaFrequency: -1,
+    teamFrequency: -1,
+    specialFrequency: -1,
+    defaultNationCount: 44,
   },
   {
     id: "China",
